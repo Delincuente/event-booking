dad156ee89740048d2b40c5202ed499c931d2fe0
CRIMINAL <butani.hardik53@gmail.com>
2025-09-23 23:33:31 +0530

    request log middleware

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,11 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+    console.log(new Date(), req.method, req.url);
+    next();
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running at port ${PORT}...`);
 });
