eedfc4cda0830d3d7bc620d14620bf07d4eaaf0e
CRIMINAL <butani.hardik53@gmail.com>
2025-09-23 23:36:32 +0530

    body parse express json middleware

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
 app.use((req, res, next) => {
     console.log(new Date(), req.method, req.url);
     next();
